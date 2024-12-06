32d2785e7a78279ad4d7a95643ba111e2f65211e
Gianluca Baldelli <92955826+Bxster@users.noreply.github.com>
2024-12-06 19:38:38 +0100

    Prolog attributes, training set and test set

---
+++ loan/prestito_attributi.pl
@@ -0,0 +1,18 @@
+% Definizione degli attributi e dei valori possibili
+a(gender, [male, female, nil]). % Genere del richiedente (Male, Female, NIL)
+a(married, [yes, no, nil]).     % Stato civile (Yes, No, NIL)
+a(dependents, [0, 1, 2, 3, nil]). % Numero di dipendenti a carico (0, 1, 2, 3, NIL)
+a(education, [graduate, not_graduate, nil]). % Livello di istruzione (Graduate, Not Graduate, NIL)
+a(self_employed, [yes, no, nil]). % Lavoratore autonomo (Yes, No, NIL)
+a(applicantincome, [low, medium_low, medium, medium_high, high, very_high, null]). % Reddito richiedente
+a(coapplicantincome, [null, low, medium_low, medium, medium_high, high]). % Reddito co-richiedente
+a(loanamount, [low, medium_low, medium, medium_high, high, very_high, nil]). % Importo del prestito
+a(loan_amount_term, [short_term, medium_short_term, medium_long_term, long_term, nil]). % Durata prestito
+a(credit_history, [0, 1, nil]). % Storico creditizio (0 = Negativo, 1 = Positivo, NIL)
+a(property_area, [urban, semiurban, rural, nil]). % Area della proprietà
+% a(loanstatus, [y, n]). Stato del prestito (Y = Approvato, N =
+% Rifiutato)
+
+
+
+
